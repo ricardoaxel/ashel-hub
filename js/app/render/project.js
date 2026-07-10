44b84a87ad14a9707ec501613f1d60b7ac86ef95
Ashel <ricardoaxelgd@gmail.com>
2026-07-09 18:54:58 -0600

    Revert "fix: remove redundant click handlers from show-more (general handler covers all)"
    This reverts commit d5005fc46f56a4b50aa3caee5f78c5edb35cb24c.

--- js/app/render/project.js
+++ js/app/render/project.js
@@ -301,6 +301,10 @@ export function renderProjectContent() {
       .join('');
     grid.insertAdjacentHTML('beforeend', extraHtml);
     photosBtn.remove();
+    document.querySelectorAll('#photos-grid .photo-card').forEach((card) => {
+      const index = parseInt(card.dataset.index, 10);
+      card.addEventListener('click', () => openModal(project.photos, index));
+    });
   });
 
   // Flyers show more
@@ -320,6 +324,10 @@ export function renderProjectContent() {
       .join('');
     grid.insertAdjacentHTML('beforeend', extraHtml);
     showMoreBtn.remove();
+    document.querySelectorAll('#flyers-grid .illustration-item').forEach((card) => {
+      const index = parseInt(card.dataset.index, 10);
+      card.addEventListener('click', () => openModal(sortedFlyers, index));
+    });
   });
 
   // Flyers initial click handler
